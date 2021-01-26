37e42da1364bcfaf680a8c00ab99e622b2f55146
ssbeast <ssbeast007@gmail.com>
2021-01-27 04:01:16 +0530

    Unnecessary comments removed

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,36 +1,28 @@
 const socket = io()
 
-// Elements
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
 
-// Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
-// Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
 const autoscroll = () => {
-    // New message element
     const $newMessage = $messages.lastElementChild
 
-    // Height of the new message
     const newMessageStyles = getComputedStyle($newMessage)
     const newMessageMargin = parseInt(newMessageStyles.marginBottom)
     const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
 
-    // Visible height
     const visibleHeight = $messages.offsetHeight
 
-    // Height of messages container
     const containerHeight = $messages.scrollHeight
 
-    // How far have I scrolled?
     const scrollOffset = $messages.scrollTop + visibleHeight
 
     if (containerHeight - newMessageHeight <= scrollOffset) {
--- src/utils/users.js
+++ src/utils/users.js
@@ -1,30 +1,25 @@
 const users = []
 
 const addUser = ({ id, username, room }) => {
-    // Clean the data
     username = username.trim().toLowerCase()
     room = room.trim().toLowerCase()
 
-    // Validate the data
     if (!username || !room) {
         return {
             error: 'Username and room are required!'
         }
     }
 
-    // Check for existing user
     const existingUser = users.find((user) => {
         return user.room === room && user.username === username
     })
 
-    // Validate username
     if (existingUser) {
         return {
             error: 'Username is in use!'
         }
     }
 
-    // Store user
     const user = { id, username, room }
     users.push(user)
     return { user }
